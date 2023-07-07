a14f59b2d67a3565e56a1d87a6e915179c026cc5
John Gardiner Myers <jgmyers@proofpoint.com>
2023-07-06 17:30:13 -0700

    hack/update-expected.sh

--- tests/integration/update_cluster/complex/kubernetes.tf
+++ tests/integration/update_cluster/complex/kubernetes.tf
@@ -1180,7 +1180,7 @@ resource "aws_subnet" "us-east-1a-utility-complex-example-com" {
 
 resource "aws_subnet" "us-test-1a-complex-example-com" {
   availability_zone                           = "us-test-1a"
-  cidr_block                                  = "172.20.32.0/19"
+  cidr_block                                  = "10.2.32.0/19"
   enable_resource_name_dns_a_record_on_launch = true
   private_dns_hostname_type_on_launch         = "resource-name"
   tags = {
